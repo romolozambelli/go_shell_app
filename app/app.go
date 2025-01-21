633cbff62e5fc95618557ad4f393bd7872c27450
Romolo Zambelli <romolo.zambelli@ionos.com>
2025-01-21 11:56:26 -0300

    Adding the new IPs lookup

--- app/app.go
+++ app/app.go
@@ -1,11 +1,46 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Return the command line application
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "First Shell Application"
 	app.Usage = "Search the Server IP on Web"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ips",
+			Usage: "Search for IPs on Internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com",
+				},
+			},
+			Action: searchIp,
+		},
+	}
+
 	return app
+}
+
+func searchIp(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
