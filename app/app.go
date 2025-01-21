abe8ea6802901116a4c06206ae0f58441c2761c1
Romolo Zambelli <romolo.zambelli@ionos.com>
2025-01-21 12:10:30 -0300

    Adding the compiled code

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "First Shell Application"
 	app.Usage = "Search the Server IP on Web"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ips",
-			Usage: "Search for IPs on Internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
+			Name:   "ips",
+			Usage:  "Search for IPs on Internet",
+			Flags:  flags,
 			Action: searchIp,
+		},
+		{
+			Name:   "server",
+			Usage:  "Search for Servers Name on Internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 
@@ -42,5 +50,19 @@ func searchIp(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host) // name server
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, serversName := range servers {
+		fmt.Println(serversName.Host)
 	}
 }
